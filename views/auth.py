406a0dcb67bb629435c3d7a8932757e1743bd2bb
Ashwin Upadhyaya <ashwin67@gmail.com>
2023-03-25 06:14:52 +0100

    Make site better

--- views/auth.py
+++ views/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -7,7 +7,7 @@ from app import db
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/register', methods=['GET', 'POST'])
+@auth.route('/register', methods=['POST'])
 def register():
     if request.method == 'POST':
         username = request.form.get('username')
@@ -17,23 +17,17 @@ def register():
         # Check if the user already exists
         user = FinUser.query.filter_by(username=username).first()
         if user:
-            flash('Username already exists. Please choose a different one.')
-            return redirect(url_for('auth.register'))
-        
+            return jsonify({'result': 'error', 'message': 'Username already exists. Please choose a different one.'})
+
         # Create a new user and save it to the database
         new_user = FinUser(username=username, email=email, password_hash=generate_password_hash(password))
         db.session.add(new_user)
         db.session.commit()
 
-        flash('Registration successful. You can now log in.')
+        return jsonify({'result': 'success'})
 
-        print("New user created: " + username)
-        print("Login path: " + url_for('auth.login'))
-        return redirect(url_for('auth.login'))
 
-    return render_template('register.html')
-
-@auth.route('/login', methods=['GET', 'POST'])
+@auth.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
@@ -45,16 +39,14 @@ def login():
         # Check if the user exists and the password is correct
         if user and check_password_hash(user.password_hash, password):
             login_user(user)
-            flash('Logged in successfully.')
-            return redirect(url_for('index'))  # Replace with the route you want to redirect the user to after login
+            return jsonify({'result': 'success'})
+        else:
+            return jsonify({'result': 'error', 'message': 'Invalid username or password.'})
 
-        flash('Invalid username or password.')
 
-    return render_template('login.html')
-
-@auth.route('/logout')
+@auth.route('/logout', methods=['POST'])
 @login_required
 def logout():
     logout_user()
     flash('Logged out successfully.')
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('index'))
